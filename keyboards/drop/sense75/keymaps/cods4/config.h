46e36aa2b565c24081cbe5dfff0457cfae48c134
cods4 <84068447+cods4@users.noreply.github.com>
2025-07-12 13:21:59 +1200

    Update config.h

--- keyboards/drop/sense75/keymaps/cods4/config.h
+++ keyboards/drop/sense75/keymaps/cods4/config.h
@@ -6,9 +6,10 @@
 #define ENABLE_RGB_MATRIX_BREATHING
 
 // Set a startup color (hue, saturation, value)
-#define RGB_MATRIX_STARTUP_HUE 0      // 0-255 (0 is red)
-#define RGB_MATRIX_STARTUP_SAT 255   // 0-255 (255 is full saturation)
-#define RGB_MATRIX_STARTUP_VAL 100   // 0-255 (255 is max brightness)
+#define RGB_MATRIX_DEFAULT_HUE 0     // 0-255 (0 is red)
+#define RGB_MATRIX_DEFAULT_SAT 255   // 0-255 (255 is full saturation)
+#define RGB_MATRIX_DEFAULT_VAL 100   // 0-255 (255 is max brightness)
+#define RGB_MATRIX_DEFAULT_SPD 127   // 0-255 (255 is max speed)
 
 // Set RGB heatmap settings
 
